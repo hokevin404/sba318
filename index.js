98ba6d7f8d9cb3b57c9dfa44a5198d339789568e
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-11 14:32:44 -0700

    Added log middleware

--- index.js
+++ index.js
@@ -4,13 +4,29 @@ const PORT = process.env.PORT || 3000;
 
 // Body parser middleware
 // Middleware to parse data within routes
+// parsed data located in "req.body"
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// New logging middleware to help us keep track of
+// requests during testing!
+app.use((req, res, next) => {
+    const time = new Date();
+  
+    console.log(
+      `-----
+  ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+    );
+    if (Object.keys(req.body).length > 0) {
+      console.log('Containing the data:');
+      console.log(`${JSON.stringify(req.body)}`);
+    }
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send("Hello HOMEPAGE!")
 })
-
 
 // Server actively listeing on port 3000
 app.listen(PORT, (req, res) => {
