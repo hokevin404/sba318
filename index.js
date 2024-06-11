4563ce361646ae50ccc0070f0791aeecb8f89bdd
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-11 14:24:02 -0700

    Added body parser middlware

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+// Body parser middleware
+// Middleware to parse data within routes
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello HOMEPAGE!")
+})
+
+
+// Server actively listeing on port 3000
 app.listen(PORT, (req, res) => {
     console.log(`Server listening on port: ${PORT}`)
 });
