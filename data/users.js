251414c9af55fcfc48e30b902c1bec00239eb251
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-11 15:16:19 -0700

    Added user data

---
+++ data/users.js
@@ -0,0 +1,26 @@
+// The "users" data will be simple information about
+// the application's user base.
+
+const users = [
+    {
+      id: 1,
+      first_name: 'Crash',
+      last_name: 'Bandicoot',
+      username: 'cyare23',
+      email: 'bandicoot.crash@wumpa.isl',
+    },
+    {
+      id: 2,
+      first_name: 'Coco',
+      last_name: 'Bandicoot',
+      email: 'bandicoot.coch@wumpa.isl',
+    },
+    {
+      id: 3,
+      first_name: 'Neo',
+      last_name: 'Cortex',
+      email: 'Neo_Cortex@iceberg.lab',
+    },
+  ];
+  
+  module.exports = users;
