c759e0a7a51dbc6d4eea6994fd89742ce630d142
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-11 21:40:48 -0700

    Added more post data

--- data/posts.js
+++ data/posts.js
@@ -12,6 +12,30 @@ const posts = [
     },
     {
         id: 2,
+        userID: 1,
+        title: "Raid of Dream: Clear - YooHyeon",
+        price: 7,
+        condition: "New",
+        description: "Official Dreamcatcher Raid of Dreams photocard of YooHyeon."
+    },
+    {
+        id: 3,
+        userID: 3,
+        title: "Raid of Dream: Clear - HanDong",
+        price: 7,
+        condition: "New",
+        description: "Official Dreamcatcher Raid of Dreams photocard of HonDong."
+    },
+    {
+        id: 4,
+        userID: 1,
+        title: "Raid of Dream: Clear - SiYeon",
+        price: 7,
+        condition: "New",
+        description: "Official Dreamcatcher Raid of Dreams photocard of SiYeon."
+    },
+    {
+        id: 5,
         userID: 2,
         title: "VersuS VillainS: Limited - Gahyeon",
         price: 10,
@@ -19,11 +43,35 @@ const posts = [
         description: "Official Dreamcatcher VersuS VillainS photocard of Gahyeon."
     },
     {
-        id: 3,
-        userID: 3,
-        title: "Apocalypse: Follow Us - JiU",
-        price: 4,
+        id: 6,
+        userID: 2,
+        title: "VersuS VillainS: Limited - DaMi",
+        price: 10,
         condition: "New",
-        deacription: "Official Dreamcatcher Apocalypse: Follow Us photocard of JiU."
+        description: "Official Dreamcatcher VersuS VillainS photocard of DaMi."
+    },
+    {
+        id: 7,
+        userID: 3,
+        title: "VersuS VillainS: Limited - SuA",
+        price: 10,
+        condition: "New",
+        description: "Official Dreamcatcher VersuS VillainS photocard of SuA."
+    },
+    {
+        id: 8,
+        userID: 3,
+        title: "VersuS VillainS: Limited - SiYeon",
+        price: 10,
+        condition: "New",
+        description: "Official Dreamcatcher VersuS VillainS photocard of SiYeon."
+    },
+    {
+        id: 9,
+        userID: 2,
+        title: "Apocalypse: Follow Us - SuA",
+        price: 7,
+        condition: "New",
+        description: "Official Dreamcatcher Apocalypse: Follow Us photocard of Sua."
     }
 ];
--- data/users.js
+++ data/users.js
@@ -2,25 +2,25 @@
 // the application's user base.
 
 const users = [
-    {
-      id: 1,
-      first_name: 'Crash',
-      last_name: 'Bandicoot',
-      username: 'cyare23',
-      email: 'bandicoot.crash@wumpa.isl',
-    },
-    {
-      id: 2,
-      first_name: 'Coco',
-      last_name: 'Bandicoot',
-      email: 'bandicoot.coch@wumpa.isl',
-    },
-    {
-      id: 3,
-      first_name: 'Neo',
-      last_name: 'Cortex',
-      email: 'Neo_Cortex@iceberg.lab',
-    },
-  ];
+  {
+    id: 1,
+    first_name: 'Crash',
+    last_name: 'Bandicoot',
+    username: 'cyare23',
+    email: 'bandicoot.crash@wumpa.isl',
+  },
+  {
+    id: 2,
+    first_name: 'Coco',
+    last_name: 'Bandicoot',
+    email: 'bandicoot.coch@wumpa.isl',
+  },
+  {
+    id: 3,
+    first_name: 'Neo',
+    last_name: 'Cortex',
+    email: 'Neo_Cortex@iceberg.lab',
+  },
+];
   
-  module.exports = users;
+module.exports = users;
