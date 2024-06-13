2bfc9518a0cd51935610f9ef9f9cf9649e695bd6
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-13 09:31:17 -0700

    Added get user route

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,10 @@ const router = express.Router();
 const users = require("../data/users.js");
 const error = require("../utilities/utilities.js");
 
+router
+    .route("/")
+    .get("/", (req, res) => {
+        res.json(users);
+    })
+
 module.exports = router;
