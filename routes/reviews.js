29fe74bf5ae2b587bc1e3039fbfcb127f7df2f8d
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-14 00:48:17 -0700

    Added error to query param

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const users = require("../data/users.js");
 const reviews = require("../data/reviews.js");
 const error = require("../utilities/utilities.js");
 
 router
     .route("/")
-    .get((req, res) => {
-        const review = reviews.filter((review) => review.toUserID == req.query.account)
-        // })
-        res.json(review);
-        
+    .get((req, res, next) => {
+        if(req.query.account <= users.length) {
+            const review = reviews.filter((review) => review.toUserID == req.query.account);
+            res.json(review);
+        } else
+            next(error(404, "Data not found"));
     });
 
 module.exports = router;
