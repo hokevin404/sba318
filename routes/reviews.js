e73e156e4f1bee6fc2a78c24888fbbf9a77356de
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-14 00:22:55 -0700

    Added GET route for reviews query

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,13 @@ const router = express.Router();
 const reviews = require("../data/reviews.js");
 const error = require("../utilities/utilities.js");
 
-
+router
+    .route("/")
+    .get((req, res) => {
+        const review = reviews.filter((review) => review.toUserID == req.query.account)
+        // })
+        res.json(review);
+        
+    });
 
 module.exports = router;
