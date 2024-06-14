f9252fa640ad79789f57eecdad2837a7db8667a7
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-13 21:02:06 -0700

    reviews route init

---
+++ routes/reviews.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+const reviews = require("../data/reviews.js");
+const error = require("../utilities/utilities.js");
+
+module.exports = router;
